d7ddd75cc678e4ade122a8c30f6e50019e943e88
Alexandre Nicastro <alexandre.nicastro@nikas.org>
2022-05-01 17:15:20 -0300

    feat(api/issues): add Cache-Control header
    Signed-off-by: Alexandre Nicastro <alexandre.nicastro@nikas.org>

--- pages/api/issues.ts
+++ pages/api/issues.ts
@@ -4,5 +4,6 @@ import { getIssues } from '../../lib/getIssues';
 export default async function verifiers(req: VercelRequest, res: VercelResponse) {
   const response = await getIssues();
 
+  res.setHeader('Cache-Control', 's-maxage=86400');
   res.status(200).json(response);
 }
