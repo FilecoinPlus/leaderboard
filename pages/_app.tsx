07dd86626f706c68cc6f8f8505be5776c566ce90
Alexandre Nicastro <alexandre.nicastro@nikas.org>
2022-07-20 16:41:32 -0300

    feat: add analytics
    Signed-off-by: Alexandre Nicastro <alexandre.nicastro@nikas.org>

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,7 @@
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
+import { useRouter } from 'next/router';
+import Script from 'next/script';
 
 // import 'antd/dist/antd.css';
 import 'antd/dist/antd.less';
@@ -7,9 +9,26 @@ import 'antd/lib/style/themes/default.less';
 
 import '@ant-design/aliyun-theme/index.less';
 
+import { useEffect } from 'react';
+
+import * as gtag from '../lib/gtag';
+
 import '../styles/variables.less';
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url);
+    };
+    router.events.on('routeChangeComplete', handleRouteChange);
+    router.events.on('hashChangeComplete', handleRouteChange);
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+      router.events.off('hashChangeComplete', handleRouteChange);
+    };
+  }, [router.events]);
+
   return (
     <>
       <Head>
@@ -19,6 +38,27 @@ function MyApp({ Component, pageProps }: AppProps) {
           content='The leaderboard for Filecoin Plus Notaries!'
         />
       </Head>
+
+      {/* Global Site Tag (gtag.js) - Google Analytics */}
+      <Script
+        strategy='afterInteractive'
+        src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
+      />
+      <Script
+        id='gtag-init'
+        strategy='afterInteractive'
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${gtag.GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+        }}
+      />
+
       <Component {...pageProps} />
     </>
   );
